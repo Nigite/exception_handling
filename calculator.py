ad90af2b6098c33b2bfab136266d6efa1565f64d
Nigite / shinn <niguel.vvicente@gmail.comgit config --global user.name Nigite>
2026-05-07 23:26:49 +0800

    succesfully integrated the ui with the calculator

--- calculator.py
+++ calculator.py
@@ -18,7 +18,7 @@ class CalculatorApp(ctk.CTk):
         super().__init__()
         self.title("Maangas Simple Calculator 3000")
         self.geometry("400x550")
-        ctk.set_appearance_mode("yellow")
+        ctk.set_appearance_mode("dark")
         
         self.operations = {
             "Addition": Addition(), "Subtraction": Subtraction(),
@@ -34,10 +34,29 @@ class CalculatorApp(ctk.CTk):
         ctk.CTkOptionMenu(self, variable=self.operation_var, values=list(self.operations.keys()), width=250).pack(pady=10)
         self.num2_entry = ctk.CTkEntry(self, placeholder_text="Enter second number", width=250)
         self.num2_entry.pack(pady=10)
-        self.calc_btn = ctk.CTkButton(self, text="Calculate", font=("Arial", 16, "bold"))
+        self.calc_btn = ctk.CTkButton(self, text="Calculate", command=self.calculate, font=("Arial", 16, "bold"))
         self.calc_btn.pack(pady=20)
         self.result_label = ctk.CTkLabel(self, text="Result will appear here", font=("Arial", 18), text_color="yellow")
         self.result_label.pack(pady=10)
+
+    def calculate(self):
+        try:
+            num1 = float(self.num1_entry.get())
+            num2 = float(self.num2_entry.get())
+            op_name = self.operation_var.get()
+
+            operation = self.operations[op_name]
+            result = operation.execute(num1, num2)
+
+            if result.is_integer(): result = int(result)
+            self.result_label.configure(text=f"Result: {result}", text_color="#00FF00")
+
+        except ValueError:
+            self.result_label.configure(text="Error: Letters not allowed!", text_color="#FF4444")
+        except ZeroDivisionError as e:
+            self.result_label.configure(text=f"Error: {e}", text_color="#FF4444")
+        except Exception:
+            self.result_label.configure(text="Unexpected Error!", text_color="#FF4444")
 
 if __name__ == "__main__":
     app = CalculatorApp()
